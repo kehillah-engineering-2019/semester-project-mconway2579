9077cb5c6bf1fd4ed301e74080bbdf7468e2db37
mconway2579 <maxlconway@gmail.com>
2018-12-13 13:25:32 -0800

    added updated code and pannel

--- sketch1/sketch1.ino
+++ sketch1/sketch1.ino
@@ -3,6 +3,7 @@
 
 #include <dht.h>
 
+unsigned long time;
 
 #define dht_apin A0
  
@@ -11,8 +12,7 @@ int temp;
 
 
 int fanpin = 8;
-int heatpin = 9;
-int heatsetting = 0;
+int transistorPin = 7;
 
 const int PulseWire = A1;
 int Threshold = 550;
@@ -30,6 +30,10 @@ void setup(){
   if (pulseSensor.begin()) {
     Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.  
   }
+
+//  pinMode(heatpin, OUTPUT);
+ // digitalWrite(heatpin, heatsetting);
+
   
   delay(1000);//Wait before accessing Sensor
 
@@ -43,19 +47,16 @@ void loop(){
  
     DHT.read22(dht_apin);
     temp = (DHT.temperature * (9/5) + (32));
-    Serial.print("Current humidity = ");
-    Serial.print(DHT.humidity);
-    Serial.print("%  ");
-    Serial.print("temperature = ");
-    Serial.print(temp); 
-    Serial.println("F  ");
-
+    
     if(temp >= 102){
       cool();
-    }
+      printTemp();
+     }
+     
     if(temp <=99){
-      heat();
-    }
+      heat();  
+      printTemp();
+     }
     pulse();
     
 
@@ -71,23 +72,34 @@ void loop(){
 
 void cool(){
   digitalWrite(fanpin, HIGH);
-  heatsetting += -10;
-  analogWrite(heatpin, heatsetting);
+  digitalWrite(transistorPin, LOW);
   }
 
 void heat(){
   digitalWrite(fanpin, LOW);
-  heatsetting += 10;
-  analogWrite(heatpin, heatsetting);
-  
+  digitalWrite(transistorPin, HIGH); 
 }
+
+void printTemp(){
+ Serial.print("Time: ");
+ time = millis();
+
+ Serial.println(time);
+
+  
+ Serial.print("%  ");
+ Serial.print("temperature = ");
+ Serial.print(temp); 
+ Serial.println("F  ");
+
+}
+
 
 void pulse(){
  int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
                                                // "myBPM" hold this BPM value now. 
 
  if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened". 
- Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
  Serial.print("BPM: ");                        // Print phrase "BPM: " 
  Serial.println(myBPM);                        // Print the value inside of myBPM. 
  }
